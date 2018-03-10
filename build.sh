a34791d444c9a904a123fdbb2a79c149c61197f8
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-03-10 11:18:08 +0100

    Update to v13.0.1RC1
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf OCP
-git clone --depth 1 --branch v13.0.0 https://github.com/nextcloud/server.git
+git clone --depth 1 --branch v13.0.1RC1 https://github.com/nextcloud/server.git
 cp -r server/lib/public OCP
 rm -rf server
