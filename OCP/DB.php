60048db3d47716b06006a6d2ecc7309f33e9636f
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-03-13 15:16:42 +0100

    Update dev-14
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- OCP/DB.php
+++ OCP/DB.php
@@ -62,39 +62,6 @@ class DB {
 	}
 
 	/**
-	 * Insert a row if the matching row does not exists.
-	 *
-	 * @param string $table The table name (will replace *PREFIX* with the actual prefix)
-	 * @param array $input data that should be inserted into the table  (column name => value)
-	 * @param array|null $compare List of values that should be checked for "if not exists"
-	 *				If this is null or an empty array, all keys of $input will be compared
-	 * @return int number of inserted rows
-	 * @throws \Doctrine\DBAL\DBALException
-	 * @deprecated 8.1.0 use insertIfNotExist() of \OCP\IDBConnection - \OC::$server->getDatabaseConnection()
-	 * @since 5.0.0 - parameter $compare was added in 8.1.0
-	 *
-	 */
-	public static function insertIfNotExist($table, $input, array $compare = null) {
-		return \OC::$server->getDatabaseConnection()->insertIfNotExist($table, $input, $compare);
-	}
-
-	/**
-	 * Gets last value of autoincrement
-	 * @param string $table The optional table name (will replace *PREFIX*) and add sequence suffix
-	 * @return string
-	 *
-	 * \Doctrine\DBAL\Connection lastInsertID()
-	 *
-	 * Call this method right after the insert command or other functions may
-	 * cause trouble!
-	 * @deprecated 8.1.0 use lastInsertId() of \OCP\IDBConnection - \OC::$server->getDatabaseConnection()
-	 * @since 4.5.0
-	 */
-	public static function insertid($table=null) {
-		return (string)\OC::$server->getDatabaseConnection()->lastInsertId($table);
-	}
-
-	/**
 	 * Start a transaction
 	 * @deprecated 8.1.0 use beginTransaction() of \OCP\IDBConnection - \OC::$server->getDatabaseConnection()
 	 * @since 4.5.0
@@ -110,27 +77,6 @@ class DB {
 	 */
 	public static function commit() {
 		\OC::$server->getDatabaseConnection()->commit();
-	}
-
-	/**
-	 * Rollback the database changes done during a transaction that is in progress
-	 * @deprecated 8.1.0 use rollback() of \OCP\IDBConnection - \OC::$server->getDatabaseConnection()
-	 * @since 8.0.0
-	 */
-	public static function rollback() {
-		\OC::$server->getDatabaseConnection()->rollBack();
-	}
-
-	/**
-	 * Check if a result is an error, works with Doctrine
-	 * @param mixed $result
-	 * @return bool
-	 * @deprecated 8.1.0 Doctrine returns false on error (and throws an exception)
-	 * @since 4.5.0
-	 */
-	public static function isError($result) {
-		// Doctrine returns false on error (and throws an exception)
-		return $result === false;
 	}
 
 	/**
--- OCP/IServerContainer.php
+++ OCP/IServerContainer.php
@@ -125,7 +125,7 @@ interface IServerContainer extends IContainer {
 	 *
 	 * @return \OCP\Files\Folder
 	 * @since 6.0.0
-	 * @deprecated since 9.2.0 use IAppData
+	 * @deprecated 9.2.0 use IAppData
 	 */
 	public function getAppFolder();
 
@@ -249,14 +249,6 @@ interface IServerContainer extends IContainer {
 	 * @since 6.0.0
 	 */
 	public function getURLGenerator();
-
-	/**
-	 * Returns the Helper
-	 *
-	 * @return \OCP\IHelper
-	 * @since 6.0.0
-	 */
-	public function getHelper();
 
 	/**
 	 * Returns an ICache instance
