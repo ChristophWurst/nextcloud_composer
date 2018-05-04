fcf58d36c5866000bb2fcb60720b9a2513a49ee8
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-05-04 19:14:06 +0200

    Set git upstream
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- push.sh
+++ push.sh
@@ -9,6 +9,8 @@ if [ ! $(git diff-index --quiet HEAD --) ]; then
 	git config --global user.name "Travis CI"
 	git add -A
 	git commit -a -m "Update OCP"
+	git remote add origin https://${GITHUB_TOKEN}@github.com/ChristophWurst/nextcloud_composer.git > /dev/null 2>&1
+	git push --quiet --set-upstream origin $TRAVIS_BRANCH
 	git push origin:$TRAVIS_BRANCH
 else
 	echo "no changes detected"
