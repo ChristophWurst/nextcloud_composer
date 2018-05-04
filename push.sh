0414f6cf199b123957539be3f84a441b710f68f1
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-05-04 19:18:19 +0200

    Restructure script
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- push.sh
+++ push.sh
@@ -2,17 +2,16 @@
 
 set -ev
 
-if [ ! $(git diff-index --quiet HEAD --) ]; then
-	echo "changes detected"
-
-	git config --global user.email "travis@travis-ci.org"
-	git config --global user.name "Travis CI"
-	git add -A
-	git commit -a -m "Update OCP"
-	git remote add origin https://${GITHUB_TOKEN}@github.com/ChristophWurst/nextcloud_composer.git > /dev/null 2>&1
-	git push --set-upstream origin $TRAVIS_BRANCH
-	git push origin:$TRAVIS_BRANCH
-else
+if git diff-index --quiet HEAD --; then
 	echo "no changes detected"
+	exit 0
 fi
+
+echo "changes detected"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git add -A
+git commit -a -m "Update OCP"
+git remote add origin https://${GITHUB_TOKEN}@github.com/ChristophWurst/nextcloud_composer.git > /dev/null 2>&1
+git push --set-upstream origin $TRAVIS_BRANCH
 
