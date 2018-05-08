9f89dd8825ef649a3128f31b22a3710c8fef6796
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-05-08 17:00:17 +0200

    Add auto-pushes
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ push.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ev
+
+if git diff-index --quiet HEAD --; then
+	echo "no changes detected"
+	exit 0
+fi
+
+echo "changes detected"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git add -A
+git commit -a -m "Update OCP"
+git remote add push-origin https://${GITHUB_TOKEN}@github.com/ChristophWurst/nextcloud_composer.git
+git push --set-upstream push-origin $TRAVIS_BRANCH
+
