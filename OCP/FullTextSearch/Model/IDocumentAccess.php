250a28dc75a5bef77a8009a2e2ccb30721727b64
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-04-08 10:08:15 +0200

    Update to 16.0.0-beta1
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- OCP/FullTextSearch/Model/IDocumentAccess.php
+++ OCP/FullTextSearch/Model/IDocumentAccess.php
@@ -31,11 +31,8 @@ declare(strict_types=1);
 namespace OCP\FullTextSearch\Model;
 
 
-use JsonSerializable;
-
-
 /**
- * Class DocumentAccess
+ * Interface IDocumentAccess
  *
  * This object is used as a data transfer object when
  *
@@ -43,321 +40,225 @@ use JsonSerializable;
  * - generating a search request.
  *
  * During the index, it is used to define which users, groups, circles, ...
- * have access to the IndexDocument
+ * have access to the IIndexDocument
  *
  * During the search, it is internally use to define to which group, circles, ...
  * a user that perform the search belongs to.
  *
- * @see IndexDocument::setAccess
+ * @see IIndexDocument::setAccess
  *
- * @since 15.0.0
+ * @since 16.0.0
  *
  * @package OCP\FullTextSearch\Model
  */
-final class DocumentAccess implements JsonSerializable {
-
-	/** @var string */
-	private $ownerId;
-
-	/** @var string */
-	private $viewerId = '';
-
-	/** @var array */
-	private $users = [];
-
-	/** @var array */
-	private $groups = [];
-
-	/** @var array */
-	private $circles = [];
-
-	/** @var array */
-	private $links = [];
+interface IDocumentAccess {
 
 
 	/**
 	 * Owner of the document can be set at the init of the object.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
-	 * DocumentAccess constructor.
+	 * IDocumentAccess constructor.
 	 *
 	 * @param string $ownerId
 	 */
-	public function __construct(string $ownerId = '') {
-		$this->setOwnerId($ownerId);
-	}
+	public function __construct(string $ownerId = '');
 
 
 	/**
 	 * Set the Owner of the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param string $ownerId
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function setOwnerId(string $ownerId) {
-		$this->ownerId = $ownerId;
-
-		return $this;
-	}
+	public function setOwnerId(string $ownerId): IDocumentAccess;
 
 	/**
 	 * Get the Owner of the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @return string
 	 */
-	public function getOwnerId(): string {
-		return $this->ownerId;
-	}
+	public function getOwnerId(): string;
 
 
 	/**
 	 * Set the viewer of the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param string $viewerId
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function setViewerId(string $viewerId): DocumentAccess {
-		$this->viewerId = $viewerId;
-
-		return $this;
-	}
+	public function setViewerId(string $viewerId): IDocumentAccess;
 
 	/**
 	 * Get the viewer of the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @return string
 	 */
-	public function getViewerId(): string {
-		return $this->viewerId;
-	}
+	public function getViewerId(): string;
 
 
 	/**
 	 * Set the list of users that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param array $users
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function setUsers(array $users): DocumentAccess {
-		$this->users = $users;
-
-		return $this;
-	}
+	public function setUsers(array $users): IDocumentAccess;
 
 	/**
 	 * Add an entry to the list of users that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param string $user
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function addUser(string $user): DocumentAccess {
-		$this->users[] = $user;
-
-		return $this;
-	}
+	public function addUser(string $user): IDocumentAccess;
 
 	/**
 	 * Add multiple entries to the list of users that have read access to the
 	 * document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param array $users
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function addUsers($users): DocumentAccess {
-		$this->users = array_merge($this->users, $users);
-
-		return $this;
-	}
+	public function addUsers($users): IDocumentAccess;
 
 	/**
 	 * Get the complete list of users that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @return array
 	 */
-	public function getUsers(): array {
-		return $this->users;
-	}
+	public function getUsers(): array;
 
 
 	/**
 	 * Set the list of groups that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param array $groups
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function setGroups(array $groups): DocumentAccess {
-		$this->groups = $groups;
-
-		return $this;
-	}
+	public function setGroups(array $groups): IDocumentAccess;
 
 	/**
 	 * Add an entry to the list of groups that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param string $group
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function addGroup(string $group): DocumentAccess {
-		$this->groups[] = $group;
-
-		return $this;
-	}
+	public function addGroup(string $group): IDocumentAccess;
 
 	/**
 	 * Add multiple entries to the list of groups that have read access to the
 	 * document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param array $groups
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function addGroups(array $groups) {
-		$this->groups = array_merge($this->groups, $groups);
-
-		return $this;
-	}
+	public function addGroups(array $groups);
 
 	/**
 	 * Get the complete list of groups that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @return array
 	 */
-	public function getGroups(): array {
-		return $this->groups;
-	}
+	public function getGroups(): array;
 
 
 	/**
 	 * Set the list of circles that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param array $circles
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function setCircles(array $circles): DocumentAccess {
-		$this->circles = $circles;
-
-		return $this;
-	}
+	public function setCircles(array $circles): IDocumentAccess;
 
 	/**
 	 * Add an entry to the list of circles that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param string $circle
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function addCircle(string $circle): DocumentAccess {
-		$this->circles[] = $circle;
-
-		return $this;
-	}
+	public function addCircle(string $circle): IDocumentAccess;
 
 	/**
 	 * Add multiple entries to the list of groups that have read access to the
 	 * document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param array $circles
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function addCircles(array $circles): DocumentAccess {
-		$this->circles = array_merge($this->circles, $circles);
-
-		return $this;
-	}
+	public function addCircles(array $circles): IDocumentAccess;
 
 	/**
 	 * Get the complete list of circles that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @return array
 	 */
-	public function getCircles(): array {
-		return $this->circles;
-	}
+	public function getCircles(): array;
 
 
 	/**
 	 * Set the list of links that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @param array $links
 	 *
-	 * @return DocumentAccess
+	 * @return IDocumentAccess
 	 */
-	public function setLinks(array $links): DocumentAccess {
-		$this->links = $links;
-
-		return $this;
-	}
+	public function setLinks(array $links): IDocumentAccess;
 
 	/**
 	 * Get the list of links that have read access to the document.
 	 *
-	 * @since 15.0.0
+	 * @since 16.0.0
 	 *
 	 * @return array
 	 */
-	public function getLinks(): array {
-		return $this->links;
-	}
+	public function getLinks(): array;
 
-
-	/**
-	 * @since 15.0.0
-	 *
-	 * @return array
-	 */
-	public function jsonSerialize(): array {
-		return [
-			'ownerId' => $this->getOwnerId(),
-			'viewerId' => $this->getViewerId(),
-			'users' => $this->getUsers(),
-			'groups' => $this->getGroups(),
-			'circles' => $this->getCircles(),
-			'links' => $this->getLinks()
-		];
-	}
 }
 
