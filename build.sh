1512560d6ca487f0dc289abe62dc8f1c403c7047
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-03-24 14:23:44 +0100

    Update to 16.0.8
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf OCP
-git clone --depth 1 --branch v16.0.7 https://github.com/nextcloud/server.git
+git clone --depth 1 --branch v16.0.8 https://github.com/nextcloud/server.git
 cp -r server/lib/public OCP
 rm -rf server
