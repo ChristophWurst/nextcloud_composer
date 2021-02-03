612aaf23a4277f77726b0bc66d674fb9f8d83670
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-02-03 08:54:38 +0100

    This is stable21
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- OCP/AppFramework/Bootstrap/IRegistrationContext.php
+++ OCP/AppFramework/Bootstrap/IRegistrationContext.php
@@ -31,6 +31,7 @@ namespace OCP\AppFramework\Bootstrap;
 
 use OCP\AppFramework\IAppContainer;
 use OCP\EventDispatcher\IEventDispatcher;
+use OCP\Files\Template\ICustomTemplateProvider;
 use OCP\IContainer;
 
 /**
@@ -119,7 +120,8 @@ interface IRegistrationContext {
 	 * @psalm-param string|class-string<T> $event preferably the fully-qualified class name of the Event sub class to listen for
 	 * @param string $listener fully qualified class name (or ::class notation) of a \OCP\EventDispatcher\IEventListener that can be built by the DI container
 	 * @psalm-param class-string<\OCP\EventDispatcher\IEventListener<T>> $listener fully qualified class name that can be built by the DI container
-	 * @param int $priority
+	 * @param int $priority The higher this value, the earlier an event
+	 *                      listener will be triggered in the chain (defaults to 0)
 	 *
 	 * @see IEventDispatcher::addServiceListener()
 	 *
@@ -195,4 +197,14 @@ interface IRegistrationContext {
 	 * @since 21.0.0
 	 */
 	public function registerWellKnownHandler(string $class): void;
+
+	/**
+	 * Register a custom template provider class that is able to inject custom templates
+	 * in addition to the user defined ones
+	 *
+	 * @param string $providerClass
+	 * @psalm-param class-string<ICustomTemplateProvider> $providerClass
+	 * @since 21.0.0
+	 */
+	public function registerTemplateProvider(string $providerClass): void;
 }
