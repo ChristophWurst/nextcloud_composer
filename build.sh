6ce63c3d29c59dc17394c8967981fd9272681713
Christoph Wurst <christoph@winzerhof-wurst.at>
2022-05-09 15:50:27 +0200

    v24.0.0
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf OCP
-git clone --depth 1 --branch v24.0.0rc2 https://github.com/nextcloud/server.git
+git clone --depth 1 --branch v24.0.0 https://github.com/nextcloud/server.git
 cp -r server/lib/public OCP
 rm -rf server
 git add OCP
